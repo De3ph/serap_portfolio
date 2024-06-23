3610b3efccd64a84a1b5c31418e38aed7167b771
De3ph <05190000108@ogrenci.ege.edu.tr>
2024-06-23 14:06:47 +0300

    feat: ui layout changes

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -1,56 +1,53 @@
 "use client"
-import { useAnimate } from "@/lib/framer"
 import serap_jpg from "@/public/serap.jpg"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/avatar"
-import { useEffect } from "react"
 
 const Index = () => {
-  const [scope, animate] = useAnimate<HTMLDivElement>()
-
-  useEffect(() => {
-    animate(
-      scope.current,
-      { opacity: [0, 1], x: [-10, 0] },
-      {
-        duration: 0.15,
-        delay: 0.1,
-        ease: "linear"
-      }
-    )
-  }, [animate, scope])
-
   return (
-    <div ref={scope} className='text-center space-y-8'>
-      <h1 className='text-3xl font-semibold'>Merhaba 👋, ben Serap</h1>
+    <div className='space-y-16 md:space-y-32'>
+      <section className='grid grid-cols-1 space-y-4 md:space-y-0 md:grid-cols-2'>
+        <div className='w-full md:flex md:flex-col md:justify-center md:items-start'>
+          <div className='text-3xl text-center'>
+            <div>
+              <span className='font-medium'>Merhaba 👋, ben</span>
+              &nbsp;
+              <span className='font-bold'>Serap</span>
+            </div>
+          </div>
+        </div>
 
-      <section className='flex justify-center'>
-        <Avatar className='size-64'>
-          <AvatarImage
-            src={serap_jpg.src}
-            alt='my_image'
-            className='object-scale-down'
-          />
-          <AvatarFallback>SF</AvatarFallback>
-        </Avatar>
+        <div>
+          <div className='w-full flex justify-end'>
+            <Avatar className='size-64'>
+              <AvatarImage
+                src={serap_jpg.src}
+                alt='my_image'
+                className='object-scale-down'
+              />
+              <AvatarFallback>SF</AvatarFallback>
+            </Avatar>
+          </div>
+        </div>
       </section>
 
-      <div className='text-xl font-medium'>
-        <p>
-          Ege Üniversitesi Görsel İletişim Tasarımı 3. sınıf öğrencisiyim.
-          Birçok sosyal alanda çalışma tecrübemin yanında bölümüm olan Görsel
-          İletişim Tasarımında da çeşitli projeler geliştirdim.
-        </p>
-        <p>
-          Hobilerim arasında vals ve tango dansları var. Ayrıca gönüllü olarak
-          birçok sosyal sorumluluk projesine katıldım. Kendimi her alanda
-          geliştirerek yeni şeyler keşfetmeyi ve seyahat etmeyi çok seviyorum.
-        </p>
-      </div>
-      <p className='text-md'>
-        Alan olarak kendime net bir alan belirlemesem de UI/UX alanında kendimi
-        geliştirmeyi seviyorum. Adobe Creative Cloud programlarına hakimim Figma
-        ise favori progralarım arasında.
-      </p>
+      <section className='space-y-8 text-wrap'>
+        <div className='text-xl font-medium space-y-4'>
+          <p>
+            Ege Üniversitesi Görsel İletişim Tasarımı 3. sınıf öğrencisiyim.
+            Birçok sosyal alanda çalışma tecrübemin yanında bölümüm olan Görsel
+            İletişim Tasarımında da çeşitli projeler geliştirdim.
+          </p>
+          <p>
+            Hobilerim arasında vals ve tango dansları var. Ayrıca gönüllü olarak
+            birçok sosyal sorumluluk projesine katıldım. Kendimi her alanda
+            geliştirerek yeni şeyler keşfetmeyi ve seyahat etmeyi çok seviyorum.
+          </p>
+        </div>
+        <div className='text-md text-balance text-center'>
+          UI/UX alanında çalışmalar yapmayı seviyorum. Adobe Creative Cloud
+          programlarına hakimim Figma ise favori progralarım arasında.
+        </div>
+      </section>
     </div>
   )
 }
